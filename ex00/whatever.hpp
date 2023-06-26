0d39f074a482be7334e4fd3614dcfc3604072bed
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2023-06-26 23:20:10 +0700

    ex00 finish

---
+++ ex00/whatever.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rthammat <rthammat@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 22:45:50 by rthammat          #+#    #+#             */
+/*   Updated: 2023/06/26 23:15:02 by rthammat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(const T &a, const T &b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+template <typename T>
+T max(const T &a, const T &b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+#endif
