88f4ea5ef798f8b08a9654c12c8a73583ad991f0
Rath Thammatucharee <rthammat@z3t4c3.42bangkok.com>
2023-06-28 21:04:14 +0700

    ex01 use test as ohm

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,23 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rthammat <rthammat@42.fr>                  +#+  +:+       +#+        */
+/*   By: rthammat <rthammat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 23:42:13 by rthammat          #+#    #+#             */
-/*   Updated: 2023/06/27 01:20:54 by rthammat         ###   ########.fr       */
+/*   Updated: 2023/06/28 21:01:51 by rthammat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main(void)
+//int main(void)
+//{
+//	int arr[] = {1, 2, 3, 4, 5};
+//	iter(arr, 5, &plus1);
+//	iter(arr, 5, &print);
+
+//	std::string arr2[] = {"abc", "def", "ghi"};
+//	iter(arr2, 3, &print);
+
+//	return (0);
+//}
+
+//template <typename T>
+//void print(const T& x)
+//{
+//    std::cout << x << " ";
+//}
+
+class Awesome
 {
-	int arr[] = {1, 2, 3, 4, 5};
-	iter(arr, 5, &plus1);
-	iter(arr, 5, &print);
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
 
-	std::string arr2[] = {"abc", "def", "ghi"};
-	iter(arr2, 3, &print);
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
 
-	return (0);
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4, 5};
+  Awesome tab2[6];
+
+  iter( tab, 6, print<const int> );
+  iter( tab2, 6, print<Awesome> );
+  return 0;
 }
